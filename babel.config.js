f58ac4f9d45ff5e21d182f1cb8ef0198ec2047a7
Septian Feri Ferdinan <feri.ferdinan79@gmail.com>
2021-06-03 17:17:41 +0700

    update

--- babel.config.js
+++ babel.config.js
@@ -2,19 +2,19 @@ module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
     [
-        'module-resolver',
-        {
-          root: ['./src'],
-          extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
-          alias: {
-            "tests": ["./tests/"],
-            "@components": "./src/components",
-            "@constants": "./src/constants",
-            "@screens": "./src/screens",
-            "@store": "./src/store",
-            "@utils": "./src/utils",
-          }
-        }
-      ]
-  ]
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          tests: ['./tests/'],
+          '@components': './src/components',
+          '@constants': './src/constants',
+          '@screens': './src/screens',
+          '@store': './src/store',
+          '@utils': './src/utils',
+        },
+      },
+    ],
+  ],
 };
